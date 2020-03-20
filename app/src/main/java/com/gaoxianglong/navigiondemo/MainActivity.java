0c8b38bb4c34ad150d8640886bd76fd5e132fb6c
Your Name <you@example.com>
2020-03-20 17:55:41 +0800

    第二次

--- app/src/main/java/com/gaoxianglong/navigiondemo/MainActivity.java
+++ app/src/main/java/com/gaoxianglong/navigiondemo/MainActivity.java
@@ -15,12 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mController = Navigation.findNavController(this,R.id.nav_host);
-        NavigationUI.setupActionBarWithNavController(this, mController);
+        mController = Navigation.findNavController(this,R.id.nav_host); // 获取管理者对象
+        NavigationUI.setupActionBarWithNavController(this, mController); // 使用导航控制器设置操作栏
     }
 
+    /**
+     * 重写onSupportNavigateUp()
+     * 设置标题栏上的返回逻辑
+     * @return 上一个导航
+     */
     @Override
     public boolean onSupportNavigateUp() {
-        return mController.navigateUp();
+        return mController.navigateUp(); // 回到上一步导航页面
     }
 }
